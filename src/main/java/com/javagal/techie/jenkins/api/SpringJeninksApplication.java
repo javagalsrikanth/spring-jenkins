29679c54f448f4366c3d4ea716115d0250ee7918
Srikanth Reddy <tmsrikanth1@gmail.com>
2020-02-08 22:09:45 +0530

    initial commit with STS

---
+++ src/main/java/com/javagal/techie/jenkins/api/SpringJeninksApplication.java
@@ -0,0 +1,25 @@
+package com.javagal.techie.jenkins.api;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJeninksApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJeninksApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started....");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application Executed....");
+		SpringApplication.run(SpringJeninksApplication.class, args);
+	}
+
+}
